19c01c0ad201fd36870cb3309d416d5af4e138b5
Junseo11 <105837745+Junseo11@users.noreply.github.com>
2022-09-21 15:44:38 +0900

    메인엑티비티 연결

--- MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/MainActivity.kt
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.lbd0.minigameparadise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var select :Button = findViewById(R.id.selectbnt)
+        var start  : Button = findViewById(R.id.startbnt)
+
+
+        select.setOnClickListener{
+            var intent : Intent = Intent(this,CharacterChoice::class.java)
+            startActivity(intent)
+        }
+
+        start.setOnClickListener{
+            var intent : Intent = Intent(this, GameChoice::class.java)
+            startActivity(intent)
+        }
+
     }
 }
