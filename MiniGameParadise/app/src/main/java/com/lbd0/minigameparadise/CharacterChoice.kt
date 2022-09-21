d80f477e27791858857aa2d7b325dfacfd043e49
BADA LEE <80818640+lbd0@users.noreply.github.com>
2022-09-21 15:26:30 +0900

    Create CharacterChoice.kt

---
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/CharacterChoice.kt
@@ -0,0 +1,11 @@
+package com.lbd0.minigameparadise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CharacterChoice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.character_choice)
+    }
+}
