5bdec5b4b4104512927876539025c96bb8ce63cb
BADA LEE <80818640+lbd0@users.noreply.github.com>
2022-09-28 10:59:27 +0900

    캐릭터 선택창 수정

--- MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/CharacterChoice.kt
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/CharacterChoice.kt
@@ -1,11 +1,23 @@
 package com.lbd0.minigameparadise
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class CharacterChoice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.character_choice)
+
+        var character : ImageView = findViewById(R.id.ani_rat)
+        val choice : Button =  findViewById<Button>(R.id.choicebtn)
+
+        choice.setOnClickListener {
+            var intent : Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
