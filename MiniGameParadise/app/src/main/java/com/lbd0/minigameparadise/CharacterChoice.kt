4dc7e1f676d80022d4712c1e100bf9a9fe8f4b39
BADA LEE <80818640+lbd0@users.noreply.github.com>
2022-09-28 12:45:58 +0900

    캐릭터 선택 창 캐릭터 클릭이벤트 완료, 캐릭터 오픈 소스 짜는중

--- MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/CharacterChoice.kt
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/CharacterChoice.kt
@@ -11,10 +11,45 @@ class CharacterChoice : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.character_choice)
 
-        var character : ImageView = findViewById(R.id.ani_rat)
-        val choice : Button =  findViewById<Button>(R.id.choicebtn)
+        var finalScore = 0 // 파이널 점수 => 일정 점수 이상이면 캐릭터 오픈
+        var index = 0   // img_view 배열 인덱스로 사용할 것
 
-        choice.setOnClickListener {
+        var character : ImageView = findViewById(R.id.presentcharac) // 현재 캐릭터 이미지뷰
+        val choice : Button =  findViewById<Button>(R.id.choicebtn) // 선택! 버튼
+
+        // 이미지뷰를 담은 배열
+        val img_view : Array<ImageView> = Array(12, {findViewById(R.id.ani_rat)})
+        // 이미지뷰에 넣을 이미지 아이디 배열
+        val img_ID = intArrayOf(R.id.ani_rat, R.id.ani_cow, R.id.ani_tiger, R.id.ani_rabbit, R.id.ani_drag, R.id.ani_sna, R.id.ani_hor, R.id.ani_sheep, R.id.ani_mon, R.id.ani_chi, R.id.ani_dog, R.id.ani_pig)
+        // 잠금해제 된 이미지 리소스
+        val img_res = intArrayOf(R.drawable.ani_rat2, R.drawable.ani_cow2, R.drawable.ani_tiger2, R.drawable.ani_rabbit2, R.drawable.ani_drag2, R.drawable.ani_sna2, R.drawable.ani_hor2, R.drawable.ani_sheep2, R.drawable.ani_mon2, R.drawable.ani_chi2, R.drawable.ani_dog2, R.drawable.ani_pig2)
+        // 현재 캐릭터 이미지뷰에 들어갈 이미지 리소스
+        val cha_res = intArrayOf(R.drawable.ani_rat, R.drawable.ani_cow, R.drawable.ani_tiger, R.drawable.ani_rabbit, R.drawable.ani_drag, R.drawable.ani_sna, R.drawable.ani_hor, R.drawable.ani_sheep, R.drawable.ani_mon, R.drawable.ani_chi, R.drawable.ani_dog, R.drawable.ani_pig)
+
+        // 반복문으로 이미지뷰 제어
+        for(i in 0 until 12) {
+            img_view[i] = findViewById<ImageView>(img_ID[i]) // 각 이미지뷰에 아이디 넣어주기
+
+            img_view[i].setOnClickListener { // 각 이미지뷰 클릭이벤트
+
+                character.setImageResource(cha_res[i])  // 클릭한 이미지로 현재 캐릭터 이미지뷰 리소스 변경
+            }
+        }
+
+        when(finalScore){
+            in 0..100 -> img_view[index].setImageResource(img_res[index++])
+            in 101..200 -> img_view[index].setImageResource(img_res[index++])
+            in 201..300 -> img_view[index].setImageResource(img_res[index++])
+            in 301..400 -> img_view[index].setImageResource(img_res[index++])
+            in 401..500 -> img_view[index].setImageResource(img_res[index++])
+
+
+
+        }
+
+
+        // 선택 버튼 이벤트
+        choice.setOnClickListener { // 선택 버튼 누르면 시작 화면으로 전환
             var intent : Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
