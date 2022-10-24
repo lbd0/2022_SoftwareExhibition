bd418254aae9bdaec5d570d51e77a188b19f81ee
Junseo11 <105837745+Junseo11@users.noreply.github.com>
2022-10-24 23:08:41 +0900

    업다운 게임 만드는중

--- MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/updownGame.kt
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/updownGame.kt
@@ -1,11 +1,85 @@
 package com.lbd0.minigameparadise
 
+import java.util.Random
+
+import android.content.Intent
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class updownGame : AppCompatActivity() {
+
+
+//    var editText : EditText= findViewById(R.id.input_text)
+//    var result : TextView = findViewById(R.id.result_txt)
+//    var life : TextView = findViewById(R.id.life_txt)
+//    var ranNum:Int?=null
+
+
+
     override fun setContentView(view: View?) {
         super.setContentView(view)
         setContentView(R.layout.updown)
+        var checkButton : Button = findViewById(R.id.c_btn)
+        var resetButton : Button = findViewById(R.id.r_btn)
+
+
+
+
+
+//        checkButton.setOnClickListener{
+//
+//
+//        }
+//
+//        resetButton.setOnClickListener{
+//
+//
+//        }
+
+
     }
+
+
+//    fun makeNum(){
+//
+//
+//        val random= Random()
+//        ranNum =random.nextInt(100)
+//
+//
+//
+//
+//    }
+
+
+
+
+//    fun compare(){  //랜덤 숫자와 사용자 입력 숫자를 비교
+//
+//        var user : String =editText.text.toString()
+//
+//        var userInt:Int=user.toInt()
+//
+//        if(userInt==ranNum){
+//            result.setText("정답")
+//
+//        }
+//
+//
+//
+//
+//
+//    }
+
+
+
+
+
+
+
+
+
 }
