9da8e62749b02f6ae8badcd67535740b002952aa
BADA LEE <80818640+lbd0@users.noreply.github.com>
2022-10-24 23:20:05 +0900

    업다운 오류 해결

--- MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/updownGame.kt
+++ MiniGameParadise/app/src/main/java/com/lbd0/minigameparadise/updownGame.kt
@@ -3,6 +3,7 @@ package com.lbd0.minigameparadise
 import java.util.Random
 
 import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -11,6 +12,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class updownGame : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.updown)
+    }
 
 //    var editText : EditText= findViewById(R.id.input_text)
 //    var result : TextView = findViewById(R.id.result_txt)
@@ -18,13 +23,13 @@ class updownGame : AppCompatActivity() {
 //    var ranNum:Int?=null
 
 
-
+/*
     override fun setContentView(view: View?) {
         super.setContentView(view)
         setContentView(R.layout.updown)
         var checkButton : Button = findViewById(R.id.c_btn)
         var resetButton : Button = findViewById(R.id.r_btn)
-
+*/
 
 
 
@@ -73,13 +78,3 @@ class updownGame : AppCompatActivity() {
 //
 //
 //    }
-
-
-
-
-
-
-
-
-
-}
